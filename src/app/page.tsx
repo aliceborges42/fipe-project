46403b6ffd914a98c394fa5d9dbfde666eaaf044
aliceborges42 <aliceborges42.ab@gmail.com>
2025-09-19 15:59:30 -0300

    feat: home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,95 @@
-import Image from "next/image";
+'use client';
+import Image from 'next/image';
+import { Button } from '@/components/ui/button';
+import FinancialSection from '../components/financiamento/financial';
+import ContatoSection from '../components/contato/contact';
+import CarrosselVeiculos from '@/components/carros/carros';
+import { useRef, useState } from 'react';
+import { Navbar } from '@/components/ui/navbar';
 
 export default function Home() {
-  return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+  const sectionRef = useRef<HTMLDivElement | null>(null);
+  const contatoRef = useRef<HTMLDivElement | null>(null);
+  const disponiveisRef = useRef<HTMLDivElement | null>(null);
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+  const [activeTab, setActiveTab] = useState<
+    'financiamento' | 'desvalorizacao'
+  >('financiamento');
+
+  function scrollToSection(
+    section: 'financiamento' | 'desvalorizacao' | 'contato' | 'disponiveis'
+  ) {
+    if (section === 'financiamento') {
+      setActiveTab('financiamento');
+      sectionRef.current?.scrollIntoView({ behavior: 'smooth' });
+    } else if (section === 'desvalorizacao') {
+      setActiveTab('desvalorizacao');
+      sectionRef.current?.scrollIntoView({ behavior: 'smooth' });
+    } else if (section === 'contato') {
+      contatoRef.current?.scrollIntoView({ behavior: 'smooth' });
+    } else if (section === 'disponiveis') {
+      disponiveisRef.current?.scrollIntoView({ behavior: 'smooth' });
+    }
+  }
+
+  return (
+    <div>
+      <Navbar onNavigate={scrollToSection} />
+      <section className="w-full">
+        <div className="grid grid-cols-1 lg:grid-cols-2 gap-8 md:gap-16 items-stretch bg-gradient-to-b from-black to-black/80">
+          <div className="p-6 md:p-10 md:pt-12 md:pr-24 space-y-6">
+            <div className="w-[200px] md:w-[300px] h-[80px] md:h-[100px] overflow-hidden relative mb-2">
+              <Image
+                src="/LogoGGbranco_amarelo.png"
+                alt="Grupo Guerra"
+                fill
+                className="object-cover object-center"
+              />
+            </div>
+            <h1 className="text-3xl md:text-4xl font-bold text-white leading-tight">
+              Venha conquistar seu carro próprio com a gente
+            </h1>
+            <p className="text-base md:text-lg text-white/90">
+              Simule aqui o financiamento do seu carro dos sonhos ou calcule a
+              projeção de desvalorização de seu carro.
+            </p>
+            <div className="flex flex-wrap gap-3 md:gap-4 my-6">
+              <Button
+                size="lg"
+                variant="secondary"
+                onClick={() => scrollToSection('financiamento')}
+              >
+                Simular Financiamento
+              </Button>
+              <Button
+                size="lg"
+                variant="default"
+                onClick={() => scrollToSection('desvalorizacao')}
+              >
+                Previsão de Desvalorização
+              </Button>
+            </div>
+          </div>
+          <div className="hidden lg:flex bg-gradient-to-b from-[#FBCE28] to-[#F5A13A] pr-10 flex-col items-center justify-center pt-16">
             <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
+              src="/car-withou-bg.png"
+              alt="Carro destaque"
+              width={1000}
+              height={1200}
+              className="w-full max-w-[800px] min-w-[520px] h-auto object-contain xl:-translate-x-20 lg:--translate-x-16"
             />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      </section>
+      <div ref={sectionRef}>
+        <FinancialSection activeTab={activeTab} />
+      </div>
+      <section ref={disponiveisRef}>
+        <CarrosselVeiculos />
+      </section>
+      <div ref={contatoRef}>
+        <ContatoSection />
+      </div>
     </div>
   );
 }
